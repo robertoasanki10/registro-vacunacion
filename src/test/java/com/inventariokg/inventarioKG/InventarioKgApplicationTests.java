0e9e7c662c2381e4467f236214bfc78099d5de3c
Roberto Salazar <r_d58@hotmail.com>
2022-08-08 03:12:03 -0500

    Funcionalidad sin seguridades aun

---
+++ src/test/java/com/inventariokg/inventarioKG/InventarioKgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventariokg.inventarioKG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioKgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
